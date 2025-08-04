4553e5b90bdc3c57f882234f302fbd90ea1700e8
Rajvickram <vikramsachin400@gmail.com>
2025-08-04 19:26:35 +0530

    Add project files.

---
+++ LoginAccessSystem/Controllers/AccountController.cs
@@ -0,0 +1,63 @@
+﻿using LoginAccessSystem.Models;
+using Microsoft.AspNetCore.Http.HttpResults;
+using Microsoft.AspNetCore.Mvc;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LoginAccessSystem.Controllers
+{
+    public class AccountController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        //We need to access the DB via EF Core, so add:-
+
+        private readonly AppDbContext _context;
+
+        public AccountController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        //Create Signup Actions :-
+        // GET: Signup form :-
+
+        public IActionResult Signup()
+        {
+            return View();
+        }
+
+        // POST: Handle Signup :-
+        [HttpPost]
+        public async Task<IActionResult> Signup(User user)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Users.Add(user);
+                await _context.SaveChangesAsync();
+                return RedirectToAction("Login");
+            }
+            return View(user);
+        }
+
+        //Create Login Actions :-
+
+
+        // GET: Login form :-
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+        // POST: Handle Login :-
+        [HttpPost]
+        public IActionResult Login(User user)
+        {
+            var checkUser = _context.Users
+
+        }
+    }
+}
---
+++ LoginAccessSystem/Models/User.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace LoginAccessSystem.Models
+{
+    public class User
+    {
+        public int Id { get; set; }
+
+        [Required]
+        public string Username { get; set; }
+
+        [Required, EmailAddress]
+        public string Email { get; set; }
+
+        [Required, DataType(DataType.Password)]
+        public string password { get; set; }
+
+    }
+}
